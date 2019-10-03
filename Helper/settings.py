a68cee9b1e96fddd790b635b624a953b80f5ad48
JohnDorian00 <remailfunny@gmail.com>
2019-10-03 15:37:58 +0300

    Update settings.py

--- Helper/settings.py
+++ Helper/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static'
-STATIC_ROOT = 'static'
+STATIC_ROOT = BASE_DIR + '/static'
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
