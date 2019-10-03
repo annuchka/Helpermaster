e9d50d5c2b5d7254bfb7175955d850086adde84f
JohnDorian00 <remailfunny@gmail.com>
2019-10-03 16:55:06 +0300

    Update settings.py

--- Helper/settings.py
+++ Helper/settings.py
@@ -18,12 +18,17 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-TEMPLATES_ROOT = BASE_DIR + '/templates'
+
 TEMPLATES_URL = '/templates/'
+TEMPLATES_ROOT = BASE_DIR + '/templates'
+
+
+STATIC_URL = '/static'
+STATIC_ROOT = BASE_DIR + '/static'
 
 # ТОЛЬКО ДЛЯ REG.RU
 TEMPLATES_ROOT.replace('\\','/')
-
+STATIC_ROOT.replace('\\','/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -128,6 +133,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static'
-STATIC_ROOT = BASE_DIR + '/static'
