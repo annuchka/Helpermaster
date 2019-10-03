6c8fffecee4931e9efcee70d3de39f31de3a9674
JohnDorian00 <remailfunny@gmail.com>
2019-10-03 15:56:06 +0300

    Update settings.py

--- Helper/settings.py
+++ Helper/settings.py
@@ -18,6 +18,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIR = (BASE_DIR + '/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'Helper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ (BASE_DIR + '/templates') ],
+        'DIRS': [ TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
